06a05b041f1f230cd6e8db56b4028bf0f233888d
1whohears <1whohears28@gmail.com>
2023-11-07 23:35:10 -0600

    added some give kit command variations

--- src/main/java/com/onewhohears/minigames/command/admin/SubComKit.java
+++ src/main/java/com/onewhohears/minigames/command/admin/SubComKit.java
@@ -27,22 +27,26 @@ public class SubComKit {
 	
 	private ArgumentBuilder<CommandSourceStack,?> giveKitArg() {
 		return Commands.literal("give")
-			.then(GameComArgs.kitNameArgument()
-				.then(Commands.argument("players", EntityArgument.players())
-				.executes(commandGiveKit(false, false, false))
-					.then(Commands.literal("give_all")
-					.executes(commandGiveKit(false, false, false)))
-					.then(Commands.literal("refill")
-					.executes(commandGiveKit(false, false, true)))
-					.then(Commands.literal("clear_other")
-					.executes(commandGiveKit(false, true, false)))
-					.then(Commands.literal("clear_all")
-					.executes(commandGiveKit(true, false, false)))
+			.then(GameComArgs.kitNameArgument().executes(commandGiveKitAll(false))
+				.then(Commands.argument("players", EntityArgument.players()).executes(commandGiveKitAll(true))
+					.then(Commands.literal("give_all").executes(commandGiveKitAll(true)))
+					.then(Commands.literal("refill").executes(commandGiveKitRefill(true)))
+					.then(Commands.literal("clear_other").executes(commandGiveKitClearOther(true)))
+					.then(Commands.literal("clear_all").executes(commandGiveKitClearAll(true)))
 				)	
 			);
 	}
 	
-	private Command<CommandSourceStack> commandGiveKit(boolean clearAll, boolean clearOther, boolean refill) {
+	private void givePlayerKit(ServerPlayer player, GameKit kit, boolean clearAll, boolean clearOther, boolean refill) {
+		if (refill) kit.giveItemsRefill(player);
+		else if (clearOther) kit.giveItemsClearOther(player);
+		else if (clearAll) kit.giveItemsClearAll(player);
+		else kit.giveItems(player);
+		Component message = Component.literal("You recieved kit "+kit.getId());
+		player.displayClientMessage(message, false);
+	}
+	
+	private Command<CommandSourceStack> commandGiveKit(boolean clearAll, boolean clearOther, boolean refill, boolean playerParam) {
 		return (context) -> {
 			String kit_name = StringArgumentType.getString(context, "kit_name");
 			GameKit kit = MiniGameKitsManager.get().getKit(kit_name);
@@ -51,25 +55,34 @@ public class SubComKit {
 				context.getSource().sendFailure(message);
 				return 0;
 			}
-			Collection<ServerPlayer> players = EntityArgument.getPlayers(context, "players");
-			for (ServerPlayer player : players) {
-				if (refill) kit.giveItemsRefill(player);
-				else if (clearOther) kit.giveItemsClearOther(player);
-				else if (clearAll) kit.giveItemsClearAll(player);
-				else kit.giveItems(player);
-				Component message = Component.literal("You recieved kit "+kit_name);
-				player.displayClientMessage(message, false);
+			if (!playerParam) {
+				givePlayerKit(context.getSource().getPlayer(), kit, clearAll, clearOther, refill);
+				return 1;
 			}
+			Collection<ServerPlayer> players = EntityArgument.getPlayers(context, "players");
+			for (ServerPlayer player : players) givePlayerKit(player, kit, clearAll, clearOther, refill);
 			Component message;
 			if (players.size() > 1) 
 				message = Component.literal("Gave "+players.size()+" players the kit "+kit_name);
 			else if (players.size() == 1) 
-				message = Component.literal("Gave "+players.iterator().next().getDisplayName()+" the kit "+kit_name);
+				message = Component.literal("Gave ").append(players.iterator().next().getDisplayName()).append(" the kit "+kit_name);
 			else 
 				message = Component.literal("No matching players found to give kit "+kit_name);
 			context.getSource().sendSuccess(message, true);
 			return 1;
 		};
+	}
+	private Command<CommandSourceStack> commandGiveKitAll(boolean playerParam) {
+		return commandGiveKit(false, false, false, playerParam);
+	}
+	private Command<CommandSourceStack> commandGiveKitRefill(boolean playerParam) {
+		return commandGiveKit(false, false, true, playerParam);
+	}
+	private Command<CommandSourceStack> commandGiveKitClearOther(boolean playerParam) {
+		return commandGiveKit(false, true, false, playerParam);
+	}
+	private Command<CommandSourceStack> commandGiveKitClearAll(boolean playerParam) {
+		return commandGiveKit(true, false, false, playerParam);
 	}
 	
 }
--- src/main/java/com/onewhohears/minigames/command/all/MiniGameAllCommands.java
+++ src/main/java/com/onewhohears/minigames/command/all/MiniGameAllCommands.java
@@ -4,12 +4,16 @@ import com.mojang.brigadier.CommandDispatcher;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 
 public class MiniGameAllCommands {
 	
 	public MiniGameAllCommands(CommandDispatcher<CommandSourceStack> d) {
-		d.register(Commands.literal("gameshop").requires(null)
-			
+		d.register(Commands.literal("gameshop").requires((stack) -> stack.hasPermission(0))
+			.executes((context) -> {
+				context.getSource().sendFailure(Component.literal("This doesn't do notn yet L!"));
+				return 1;
+			})
 		);
 	}
 	
