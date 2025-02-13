706be2861416f206244f95777f549de62c5337c0
1whohears <1whohears28@gmail.com>
2025-02-12 23:51:09 -0600

    added /minigame override set_lives command

--- src/main/java/com/onewhohears/minigames/command/admin/MiniGameAdminCommands.java
+++ src/main/java/com/onewhohears/minigames/command/admin/MiniGameAdminCommands.java
@@ -9,13 +9,14 @@ public class MiniGameAdminCommands {
 	
 	public MiniGameAdminCommands(CommandDispatcher<CommandSourceStack> d) {
 		d.register(Commands.literal("minigame").requires((stack) -> stack.hasPermission(2))
-			.then(new SubComSetup().setup())
-			.then(new SubComCreateNew().createNew())
-			.then(new SubComReset().reset())
-			.then(new SubComRemove().remove())
-			.then(new SubComInfo().info())
-			.then(new SubComKit().kit())
-			.then(new SubComShop().shop())
+				.then(new SubComSetup().setup())
+				.then(new SubComCreateNew().createNew())
+				.then(new SubComReset().reset())
+				.then(new SubComRemove().remove())
+				.then(new SubComInfo().info())
+				.then(new SubComKit().kit())
+				.then(new SubComShop().shop())
+				.then(new SubComOverride().override())
 		);
 	}
 	
---
+++ src/main/java/com/onewhohears/minigames/command/admin/SubComOverride.java
@@ -0,0 +1,51 @@
+package com.onewhohears.minigames.command.admin;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.onewhohears.minigames.command.GameComArgs;
+import com.onewhohears.minigames.minigame.MiniGameManager;
+import com.onewhohears.minigames.minigame.agent.PlayerAgent;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SubComOverride {
+
+	public SubComOverride() {
+	}
+	
+	public ArgumentBuilder<CommandSourceStack,?> override() {
+		return Commands.literal("override")
+				.then(Commands.literal("set_lives")
+						.then(Commands.argument("players", EntityArgument.players())
+								.then(Commands.argument("lives", IntegerArgumentType.integer(0))
+									.executes(setLives()))));
+	}
+
+	private Command<CommandSourceStack> setLives() {
+		return (context) -> {
+			Collection<ServerPlayer> players = EntityArgument.getPlayers(context, "players");
+			int lives = IntegerArgumentType.getInteger(context, "lives");
+			for (ServerPlayer player : players) {
+				List<PlayerAgent> agents = MiniGameManager.get().getActiveGamePlayerAgents(player);
+				if (agents.isEmpty()) {
+					Component message = Component.literal(player.getDisplayName()+" is not in any active games!");
+					context.getSource().sendFailure(message);
+					continue;
+				}
+				for (PlayerAgent agent : agents) {
+					agent.setLives(lives);
+					Component message = Component.literal("Set "+player.getDisplayName()+"'s lives to "+lives+"!");
+					context.getSource().sendSuccess(message, true);
+				}
+			}
+			return 1;
+		};
+	}
+}
