d532f7406e3b2b0515a81883e07043cb9196d924
1whohears <1whohears28@gmail.com>
2025-02-17 19:01:35 -0600

    gave event item a texture, has dynamic name based on event type, and tooltips show event parameters.

--- src/main/java/com/onewhohears/minigames/item/EventItem.java
+++ src/main/java/com/onewhohears/minigames/item/EventItem.java
@@ -3,14 +3,19 @@ package com.onewhohears.minigames.item;
 import com.onewhohears.minigames.minigame.MiniGameManager;
 import com.onewhohears.onewholibs.util.UtilMCText;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 import static com.onewhohears.minigames.minigame.data.MiniGameData.RED;
 
@@ -42,5 +47,24 @@ public class EventItem extends Item {
     private InteractionResultHolder<ItemStack> sendError(Player player, ItemStack stack, String msg) {
         player.sendSystemMessage(UtilMCText.literal(msg).setStyle(RED));
         return InteractionResultHolder.fail(stack);
+    }
+
+    @Override
+    public @NotNull Component getName(@NotNull ItemStack stack) {
+        if (stack.getTag() == null) return UtilMCText.translatable("event.minigames.not_registered");
+        String event = stack.getTag().getString("event");
+        if (event.isEmpty()) return UtilMCText.translatable("event.minigames.not_registered");
+        return UtilMCText.translatable("event.minigames."+event);
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level,
+                                @NotNull List<Component> tips, @NotNull TooltipFlag isAdvanced) {
+        super.appendHoverText(stack, level, tips, isAdvanced);
+        if (stack.getTag() == null || !stack.getTag().contains("params")) return;
+        CompoundTag params = stack.getTag().getCompound("params");
+        for (String key : params.getAllKeys()) {
+            tips.add(UtilMCText.literal(key+": "+params.get(key)));
+        }
     }
 }
