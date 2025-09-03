7a1fb5af509bc97db7a5010eca41f49b69374f67
1whohears <79810154+1whohears@users.noreply.github.com>
2025-09-03 13:39:10 -0500

    update 1wholibs

--- fabric/src/main/java/com/onewhohears/minigames/fabric/MiniGamesModFabric.java
+++ fabric/src/main/java/com/onewhohears/minigames/fabric/MiniGamesModFabric.java
@@ -1,11 +1,16 @@
 package com.onewhohears.minigames.fabric;
 
 import com.onewhohears.minigames.MiniGamesMod;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
 import net.fabricmc.api.ModInitializer;
 
 public class MiniGamesModFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         MiniGamesMod.init();
+        if (Platform.getEnvironment() == Env.CLIENT) {
+            MiniGamesMod.clientInit();
+        }
     }
 }
